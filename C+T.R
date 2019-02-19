c7775339c1c031fad30f0e442c1d577e353ffc6c
Florian Privé <florian.prive@inp-grenoble.org>
2019-02-19 17:48:02 +0100

    new simu

--- C+T.R
+++ C+T.R
@@ -10,30 +10,31 @@ BETA <- sumstats$beta
 LPVAL <- -log10(sumstats$pval)
 
 library(ggplot2)
-qplot(y = LPVAL) + ylim(1, NA)
+# qplot(y = LPVAL) + ylim(1, NA)
 THR <- exp(seq(log(0.01), log(0.999 * max(LPVAL)), length.out = 50))
 hist(LPVAL); abline(v = THR, lty = 3)
 
-ind.keep <- snp_clumping(G, infos.chr = CHR, S = LPVAL, thr.r2 = 0.05,
+ind.keep <- snp_clumping(G, infos.chr = CHR, S = LPVAL, thr.r2 = 0.05, size = 8000,
                          is.size.in.bp = TRUE, infos.pos = POS,
                          ncores = nb_cores())
 qplot(ind.keep, LPVAL[ind.keep]) + ylim(1, NA) +
   geom_hline(yintercept = -log10(5e-8), color = "red", linetype = 2)
 
-# Why negative?
-prs.train <- snp_PRS(train$genotypes, -BETA[ind.keep], ind.keep = ind.keep,
+
+prs.train <- snp_PRS(train$genotypes, BETA[ind.keep], ind.keep = ind.keep,
                      lpS.keep = LPVAL[ind.keep], thr.list = THR)
-auc.train <- apply(prs.train, 2, AUC, train$fam$affection - 1)
+auc.train <- apply(prs.train, 2, AUC, train$fam$affection)
 plot(auc.train, pch = 20)
-AUCBoot(prs.train[, 1], train$fam$affection - 1)  ## 68.5 [67.0-69.9]
+# AUCBoot(prs.train[, 1], train$fam$affection - 1)  ## 68.5 [67.0-69.9]
 
 # snp_readBed("data/data_test.bed")
 test <- snp_attach("data/data_test.rds")
-prs.test <- snp_PRS(test$genotypes, -BETA[ind.keep], ind.keep = ind.keep,
+prs.test <- snp_PRS(test$genotypes, BETA[ind.keep], ind.keep = ind.keep,
                     lpS.keep = LPVAL[ind.keep], thr.list = THR)
 dim(prs.test) # 2000 x 50
 auc.test <- apply(prs.test, 2, AUC, test$fam$affection)
 plot(auc.test, pch = 20)
 
-AUCBoot(prs.test[, which.max(auc.train)], test$fam$affection)  ## 69.6 [66.8-72.4]
+AUCBoot(prs.test[, which.max(auc.train)], test$fam$affection)
+# 75.0 [72.4.8-77.6] (2000) || 75.2 [72.6-77.8] (4000)
 
---
+++ get_data.sh
@@ -0,0 +1,2 @@
+scp -r privef@krakenator:~/UKBiobank/simus/data_t*{.bed,.bim,.fam} data/
+scp -r privef@krakenator:~/UKBiobank/simus/sumstats.txt data/
