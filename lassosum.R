5f536971dcccfd90d2971bd4d330fe97259cf378
Florian Privé <florian.prive@inp-grenoble.org>
2019-03-25 11:21:29 +0100

    save

--- lassosum.R
+++ lassosum.R
@@ -25,6 +25,21 @@ test <- snp_attach("data/data_test.rds")
 length(ind <- which(v$best.beta != 0))
 pred <- big_prodVec(test$genotypes, v$best.beta[ind], ind.col = ind)
 AUCBoot(pred, test$fam$affection)
-
 ## New simu: 72.9 [70.1-75.5]
 ## Previous simu: 72.7 [69.9-75.4]
+
+plot(sumstats$beta[ind], v$best.beta[ind], pch = 20); abline(0, 1, col = "red")
+
+# pROC::roc(cases = pred[test$fam$affection == 1],
+#           controls =  pred[test$fam$affection == 0], ci=TRUE)
+#
+# mod <- glm(test$fam$affection ~ pred, family = "binomial")
+# cat("Nagelkerke's R2 =", DescTools::PseudoR2(mod, "Nagelkerke"), "\n")
+#
+# pred2 <- 0.001 * pred
+# mod2 <- glm(test$fam$affection ~ pred2, family = "binomial")
+# cat("Nagelkerke's R2 =", DescTools::PseudoR2(mod2, "Nagelkerke"), "\n")
+#
+# pred3 <- 1 / (1 + exp(-pred))
+# mod3 <- glm(test$fam$affection ~ pred3, family = "binomial")
+# cat("Nagelkerke's R2 =", DescTools::PseudoR2(mod3, "Nagelkerke"), "\n")
--- oracle-AUC.R
+++ oracle-AUC.R
@@ -1,10 +1,11 @@
-K <- 0.2
-sapply(c(0.5, 0.6, 0.8), function(h2) {
-  T0 <- qnorm(1 - K)
-  z <- dnorm(T0)
-  i <- z / K
-  v <- -i * K / (1 - K)
-  num <- (i - v) * h2
-  deno.part <- 1 - h2 * i * (i - T0) + (1 - h2 * v * (v - T0))
-  pnorm(num / sqrt(h2 * deno.part))
+sapply(setNames(nm = c(0.05, 0.1, 0.2)), function(K) {
+  sapply(setNames(nm = c(0.3, 0.5, 0.6, 0.8)), function(h2) {
+    T0 <- qnorm(1 - K)
+    z <- dnorm(T0)
+    i <- z / K
+    v <- -i * K / (1 - K)
+    num <- (i - v) * h2
+    deno.part <- 1 - h2 * i * (i - T0) + (1 - h2 * v * (v - T0))
+    round(100 * pnorm(num / sqrt(h2 * deno.part)), 2)
+  })
 })
