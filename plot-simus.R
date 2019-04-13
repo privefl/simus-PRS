9884701873e9c6fd719146697e95144589cd58d3
Florian Privé <florian.prive.21@gmail.com>
2019-04-13 09:21:49 +0200

    save

--- plot-simus.R
+++ plot-simus.R
@@ -11,7 +11,7 @@ auc_simu <- res_simu %>%
   select(1, 2, 4, 6) %>%
   group_by(simu) %>%
   summarise_all(~ {
-    boot <- replicate(1e3, mean(sample(.x, replace = TRUE)))
+    boot <- replicate(1e4, mean(sample(.x, replace = TRUE)))
     q <- quantile(boot, c(0.025, 0.975))
     res <- c(mean = mean(boot), inf = q[[1]], sup = q[[2]])
     list(res)
@@ -19,13 +19,19 @@ auc_simu <- res_simu %>%
   gather("Method", "AUC", -simu) %>%
   mutate(AUC = map(AUC, ~as_tibble(as.list(.x))),
          Method = ordered(Method, levels = c("auc_std_prs", "auc_max_prs", "auc_SCT"),
-                          labels = c("std_PRS", "max_PRS", "SCT"))) %>%
+                          labels = c("std_CT", "max_CT", "SCT"))) %>%
   unnest()
 
 ggplot(auc_simu, aes(simu, mean, fill = Method, color = Method)) +
   bigstatsr::theme_bigstatsr() +
+  geom_hline(yintercept = 0.5, linetype = 2) +
+  geom_hline(yintercept = 0.875, linetype = 3, color = "blue") +
   geom_col(position = position_dodge(), alpha = 0.5, color = "black", size = 1) +
   geom_errorbar(aes(ymin = inf, ymax = sup),
                 position = position_dodge(width = 0.9), color = "black", width = 0.2, size = 1) +
   scale_y_continuous(limits = c(0.5, NA), minor_breaks = 0:20 / 20,
-                     oob = scales::rescale_none)
+                     oob = scales::rescale_none) +
+  labs(x = "Simulation", y = "AUC") +
+  theme(legend.position = c(0.4, 0.87))
+
+ggsave("figures/AUC-simus.pdf", width = 900, height = 600, scale = 1 / 100)
